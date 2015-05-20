7f652c9474e090b5e064497da49991b8b533c194
leebo <me@bobo.im>
2015-05-20 16:15:26 +0800

    增加http basic 用户认证

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  http_basic_authenticate_with name: "bobo", password: "lendlove"
 
   # GET /users
   # GET /users.json
