e1a646f1445255d0d8d62fd862ff5d7081089bd2
leebo <me@bobo.im>
2015-05-21 18:36:03 +0800

    修复正则表达式验证手机号小bug  需要在开始结尾处加入\A \z

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User
   validates :mobile, :sn, presence: true
   validates :sn, format: { with: /\d{8}/ }
   validates :mobile,  uniqueness: { message: "你已经领取过，每人只能领取一次,请查收你的短信,或者联系我们" }
-  validates :mobile, format: { with: /1[3|4|5|7|8][0-9]\d{8}/ , message: "手机号码输入不正确" }
+  validates :mobile, format: { with: /\A1[3|4|5|7|8][0-9]\d{8}\z/ , message: "手机号码输入不正确" }
 
   def gen_sn
     self.sn = rand(99999999)
